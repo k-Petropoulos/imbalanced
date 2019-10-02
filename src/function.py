6ac31e6a7ad3bb1dc8271a8bc9f3ae95ae2bb1e1
alexandre-minydetornaco <alexandre.miny@hotmail.com>
2019-10-02 18:51:44 +0200

    get dataset

--- src/function.py
+++ src/function.py
@@ -1,3 +1,15 @@
-pip install imbalanced-learn
-
 import imblearn
+from sklearn.model_selection import train_test_split
+import numpy as np
+import pandas as pd
+import os
+import sys
+import seaborn as sns
+import matplotlib as plt
+
+def getdataset(df):
+    X = df.iloc[:,:-1]
+    y = df['Class']
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    return (X_train, X_test, y_train, y_test)
+
