00cc10c07f7477ffd98e080d03accc32aa5ca1cb
alexandre-minydetornaco <alexandre.miny@hotmail.com>
2019-10-02 19:48:38 +0200

    add ElasticNet function

--- src/function.py
+++ src/function.py
@@ -6,6 +6,7 @@ import os
 import sys
 import seaborn as sns
 import matplotlib as plt
+from sklearn.linear_model import ElasticNetCV
 
 def getdataset(df):
     X = df.iloc[:,:-1]
@@ -13,3 +14,23 @@ def getdataset(df):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
     return (X_train, X_test, y_train, y_test)
 
+def elasticNet (X_train, y_train, X_test):
+    elasticnet = ElasticNetCV(l1_ratio=[.1, .5, .7, .9, .95, .99, 1],
+                          eps=0.001,
+                          n_alphas=100,
+                          alphas=None,
+                          fit_intercept=True,
+                          normalize=False,
+                          precompute='auto',
+                          max_iter=1000,
+                          tol=0.0001,
+                          cv=4,
+                          copy_X=True,
+                          verbose=0,
+                          n_jobs=None,
+                          positive=False,
+                          random_state=None,
+                          selection='cyclic')
+    elasticnet.fit(X_train,y_train)
+    y_pred = elasticnet.predict((X_test))
+    return y_pred
