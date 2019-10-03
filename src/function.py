1c5e6979f8f09a7c554a57a8dad5c4a95d04e98c
alexandre-minydetornaco <alexandre.miny@hotmail.com>
2019-10-03 20:55:28 +0200

    add stratify parameter

--- src/function.py
+++ src/function.py
@@ -35,7 +35,7 @@ from inspect import signature
 def getdataset(df):
     X = df.iloc[:,:-1]
     y = df['Class']
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42, stratify=y)
     return (X_train, X_test, y_train, y_test)
 
 ############# Metrics #############
